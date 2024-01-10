ea102909a63e5b387f9cd83088f6d8985f78ca93
D9eka <120364200+D9eka@users.noreply.github.com>
2024-01-10 10:52:17 +0500

    Добавление концовки и титров

---
+++ Assets/Scripts/Components/UI/EndGameScreen.cs
@@ -0,0 +1,33 @@
+using Creatures.Player;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EndGameScreen : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI _playerProgress;
+    [SerializeField] private TextMeshProUGUI _endGameText;
+    [Space]
+    [SerializeField] private string _zeroPercentText;
+    [SerializeField] private string _fourtyPercentText;
+    [SerializeField] private string _seventyPercentText;
+    [SerializeField] private string _hundredPercentText;
+
+    private void Start()
+    {
+        float playerProgress = PlayerPrefsController.GetPlayerProgress();
+        _playerProgress.text = $"{playerProgress}%";
+        LayoutRebuilder.ForceRebuildLayoutImmediate(_playerProgress.transform.parent.GetComponent<RectTransform>());
+
+        if (playerProgress <= 42f)
+            _endGameText.text = _zeroPercentText;
+        else if (playerProgress <= 70f)
+            _endGameText.text = _fourtyPercentText;
+        else if (playerProgress <= 99f)
+            _endGameText.text = _seventyPercentText;
+        else
+            _endGameText.text = _hundredPercentText;
+    }
+}
